e46b5cec27cc2dbb5246c4d6f0b9fd14250bd8dd
ArvindGz-coder <arvind1306gupta@gmail.com>
2026-03-19 09:14:36 +0530

    first commit

---
+++ 20-01 L-1/app.js
@@ -0,0 +1,9 @@
+//importing the exported object
+const data=require("./exportdata");
+// const file = require("jjjjj");
+
+console.log("Students Details: ");
+console.log("ID: ",data.student.id);
+console.log("Name: ",data.student.name);
+console.log("Course: ",data.student.course);
+console.log("Marks: ",data.student.marks);
---
+++ 20-01 L-1/exportdata.js
@@ -0,0 +1,6 @@
+exports.student = {
+    id:101,
+    name:"Shubham",
+    course:"full stack",
+    marks:80
+};
